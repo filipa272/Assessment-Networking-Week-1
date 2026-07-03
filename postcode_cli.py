d5f4202aed8db8dfe56f4839210d777f9f5fa4d4
Filipa Fonseca <filipa.m.freire@gmail.com>
2026-07-03 16:39:03 +0100

    task 2 completed

--- postcode_cli.py
+++ postcode_cli.py
@@ -1,7 +1,27 @@
 """A CLI application for interacting with the Postcode API."""
 
 from argparse import ArgumentParser
+from postcode_functions import get_postcodes_details, validate_postcode, get_postcode_for_location, get_postcode_completions
 
 
 if __name__ == "__main__":
-    pass
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--mode", "-m", choices=['validate', 'complete'], required=True)
+    parser.add_argument("postcode", type=str)
+    args = parser.parse_args()
+
+    if args.mode == "validate":
+        if validate_postcode(args.postcode):
+            print(f'{args.postcode.upper().strip()} is a valid postcode.')
+        else:
+            print(f"{args.postcode.upper().strip()} is not a valid postcode.")
+
+    if args.mode == "complete":
+
+        if get_postcode_completions(args.postcode):
+            for postcode in get_postcode_completions(args.postcode)[:5]:
+                print(f"{postcode.upper().strip()}.")
+        else:
+
+            print(f'No matches for {args.postcode.upper().strip()}.')
